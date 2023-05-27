fe89c451871fffdf3792ee1f8a63dc19d1966e76
Alex Patapan <alexpatapan@gmail.com>
2023-05-27 11:46:35 +1000

    rename file

--- rust/dex/src/main.rs
+++ rust/dex/src/main.rs
@@ -1,12 +1,16 @@
 // main.rs
-mod exchange;
-use exchange::get_jupiter_price;
+mod exchanges;
 
 #[tokio::main]
 async fn main() {
     let client = reqwest::Client::new();
 
-    let price = get_jupiter_price(&client, "So11111111111111111111111111111111111111112", "4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R").await.expect("Failed to get price");
+    let token_addresses = exchange::get_token_ids_from_txt().unwrap();
+
+    let SOL = token_addresses.get("SOL").expect("panic");
+    let RAY = token_addresses.get("RAY").expect("panic");
+
+    let price = exchange::get_jupiter_price(&client, SOL, RAY).await.expect("Failed to get price");
 
     println!("{}", price);
 }
